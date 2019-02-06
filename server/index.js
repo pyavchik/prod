6935d24cfa68c0b8ff0961a62ec7f4eddb9e6ad1
pyavchik@gmail.com <pyavchik@gmail.com>
2019-02-06 21:11:25 +0200

    added passport dependency

--- server/index.js
+++ server/index.js
@@ -1,8 +1,31 @@
-const express = require('express');
+const express = require('express'),
+    passport = require('passport'),
+    GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
+//passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://www.example.com/auth/google/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({hi: 'there!'});
+});
+
+app.get('/hi', (req, res) => {
+    res.send({hi: 'there!'});
+});
+
+app.get('/bye', (req, res) => {
+    res.send({bye: 'body!'});
 });
 
 const PORT  = process.env.PORT || 5000;
