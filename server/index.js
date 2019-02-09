a298ca2dea4f7251af20d23d172026b07599e758
pyavchik@gmail.com <pyavchik@gmail.com>
2019-02-09 16:56:59 +0200

    update

--- server/index.js
+++ server/index.js
@@ -1,32 +1,10 @@
-const express = require('express'),
-    passport = require('passport'),
-    GoogleStrategy = require('passport-google-oauth20').Strategy;
+const express = require('express');
+const passportConfig = require('./services/passport');
+
 
 const app = express();
-//passport.use(new GoogleStrategy());
-passport.use(new GoogleStrategy({
-        clientID: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://www.example.com/auth/google/callback"
-    },
-    function(accessToken, refreshToken, profile, cb) {
-        User.findOrCreate({ googleId: profile.id }, function (err, user) {
-            return cb(err, user);
-        });
-    }
-));
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there!'});
-});
-
-app.get('/hi', (req, res) => {
-    res.send({hi: 'there!'});
-});
-
-app.get('/bye', (req, res) => {
-    res.send({bye: 'body!'});
-});
+const authRoutes = require('./routes/authRoutes')(app);
 
 const PORT  = process.env.PORT || 5000;
 app.listen(PORT);
