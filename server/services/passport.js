467ae7d8a04c6a0075e7af100f66528777d0a5af
pyavchik@gmail.com <pyavchik@gmail.com>
2019-02-09 23:45:37 +0200

    update

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,20 @@
-passport = require('passport');
-GoogleStrategy = require('passport-google-oauth20').Strategy;
-keys = require('../config/keys');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+});
 
 passport.use(new GoogleStrategy({
         clientID: keys.googleClientID ,
@@ -8,8 +22,14 @@ passport.use(new GoogleStrategy({
         callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) =>  {
-        console.log('access token', accessToken);
-        console.log('refreshT token', refreshToken);
-        console.log('profile', profile);
+        User.findOne({ googleId: profile.id }).then((existingUser) => {
+                if (!existingUser){
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
+                } else {
+                    done(null, existingUser);
+                }
+            });
     }
 ));
